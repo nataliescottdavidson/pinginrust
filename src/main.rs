f3876ca9febaf67c73c51f8e01796091a7bc3121
Natalie Davidson <davidsonnatalies@gmail.com>
2020-04-15 20:43:54 +0000

    Not working but want to work on in local environment now

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use smoltcp::wire::{Ipv4Address};
 use validators::ValidatorOption;
 use validators::ipv4::{IPv4Validator};
+use validators::domain::{DomainValidator};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,8 +14,21 @@ fn main() {
         local: ValidatorOption::NotAllow,
         ipv6: ValidatorOption::Allow
     };
+    let domain = DomainValidator {
+        port: ValidatorOption::NotAllow,
+        localhost: ValidatorOption::NotAllow
+    }
+    let addr_ = ip.parse_string(addr);
+    if addr_.is_ok() {
+        assert_eq!("1.1.1.1", addr_.unwrap());
+    }
+    else {
+        let addr__ = domain.parse_string(addr);
+        if addr__.is_ok() {
+            assert_eq!( "google.com", addr.unwrap()
 
-    let addr2 = ip.parse_string(addr).unwrap();
+        }
+    }
 }
 
 
