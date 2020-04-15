1b164995484fa8dd0115fd1b4e312841bbdfa4f2
Natalie Davidson <davidsonnatalies@gmail.com>
2020-04-15 20:10:42 +0000

    IPV4 validator works

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 use std::env;
+use smoltcp::wire::{Ipv4Address};
+use validators::ValidatorOption;
+use validators::ipv4::{IPv4Validator};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
-    let hostname = &args[1]
+   
+    let addr = args[1].to_string();
 
-    println!("{:?}", args);
+    let ip = IPv4Validator {
+        port: ValidatorOption::NotAllow,
+        local: ValidatorOption::NotAllow,
+        ipv6: ValidatorOption::Allow
+    };
+
+    let addr2 = ip.parse_string(addr).unwrap();
 }
+
+
+
