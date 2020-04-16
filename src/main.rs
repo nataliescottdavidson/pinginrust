36722fe2a889cd0cd6ea264d53077b060fce5289
System Administrator <root@nsd.local>
2020-04-16 10:16:11 -0400

    Huzzah! successfully sent echo request. Did it at layer 4 which I may have to change to something lower level in order to receive the reply packets.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
 use std::env;
-use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
+use std::net::IpAddr;
 use validators::ValidatorOption;
 use validators::ipv4::{IPv4Validator};
 use validators::domain::{Domain, DomainValidator};
 use dns_lookup::lookup_host;
-use pnet::packet::{MutablePacket, Packet};
-use pnet::transport::TransportChannelType::Layer3;
+use pnet::packet::Packet;
+use pnet::transport::TransportChannelType::Layer4;
 use pnet::transport::TransportProtocol::Ipv4;
 use pnet::transport::transport_channel;
-use pnet_packet::ip::IpNextHeaderProtocols;
-use pnet_packet::icmp::{Icmp, IcmpType, IcmpCode};
-
-extern crate pnet_datalink;
+use pnet::packet::ip::IpNextHeaderProtocols;
+use pnet::packet::icmp::echo_request::{MutableEchoRequestPacket};
+use pnet::packet::icmp::{IcmpCode, IcmpPacket, IcmpTypes, checksum};
 
 fn dns(domain : Domain) -> IpAddr {
     match lookup_host(domain.get_full_domain()) {
@@ -36,10 +35,6 @@ fn get_ip_from_raw_addr(raw_addr : &String) -> IpAddr {
         localhost: ValidatorOption::NotAllow
     };
 
-    for interface in pnet_datalink::interfaces() {
-        println!("{}", interface);
-    }
-
     match ipv4.parse_string(raw_addr.clone()) {
         Ok(ipv4_addr) => {
             assert_eq!(raw_addr, ipv4_addr.get_full_ipv4());
@@ -59,32 +54,29 @@ fn get_ip_from_raw_addr(raw_addr : &String) -> IpAddr {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-   
     let raw_addr = args[1].to_string().clone();
-
     let ip_addr = get_ip_from_raw_addr(&raw_addr);
 
-
-    // Create a new transport channel, dealing with layer 4 packets on a test protocol
-    // It has a receive buffer of 4096 bytes.
-    let (mut sender, mut reciever) = match transport_channel(4096, Layer3(IpNextHeaderProtocols::Icmp)) {
+    let (mut sender, _) = match transport_channel(4096, Layer4(Ipv4(IpNextHeaderProtocols::Icmp))) {
         Ok((tx, rx)) => (tx, rx),
         Err(e) => panic!(
             "An error occurred when creating the transport channel: {}",
             e
         ),
     };
-    let mut vec = Vec::new();
-    vec.push(1);
 
-    let packet = Icmp {
-        icmp_type : IcmpType(8),
-        icmp_code : IcmpCode(0),
-        checksum : 0,
-        payload : vec
-    };
+    let mut buffer = [0u8; 42];
+    let mut packet = MutableEchoRequestPacket::new(&mut buffer).unwrap();
+    packet.set_sequence_number(0);
+    packet.set_icmp_type(IcmpTypes::EchoRequest);
+    packet.set_icmp_code(IcmpCode::new(0));
+    let echo_checksum = checksum(&IcmpPacket::new(packet.packet()).unwrap());
+    packet.set_checksum(echo_checksum);
 
-
+    match sender.send_to(packet, ip_addr) {
+        Ok(_size) => println!("Sent successfully"),
+        Err(e) => println!("{:?}", e)
+    }
 
 }
 
