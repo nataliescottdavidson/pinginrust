12798ca816f3452bc0bbf998671c0882297b4171
nataliescottdavidson <davidsonnatalies@gmail.com>
2020-04-15 21:41:17 -0400

    need to parse ip from dns lookup

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 use validators::ValidatorOption;
 use validators::ipv4::{IPv4Validator};
 use validators::domain::{Domain, DomainValidator};
@@ -8,21 +9,21 @@ use pnet::transport::TransportChannelType::Layer3;
 use pnet::transport::TransportProtocol::Ipv4;
 use pnet::transport::transport_channel;
 use pnet_packet::ip::IpNextHeaderProtocols;
+use pnet_packet::icmp::{Icmp, IcmpType, IcmpCode};
 
 extern crate pnet_datalink;
 
 fn dns(domain : Domain) {
     match lookup_host(domain.get_full_domain()) {
-        Ok(ips) => println!("{:?}", ips),
-        Err(_) => println!("DNS lookup did not resolve")
+        Ok(ips) => {
+            println!("{:?}", ips);
+        }
+        Err(_) => panic!("DNS lookup did not resolve")
      }
+
 }
 
-
-fn main() {
-    let args: Vec<String> = env::args().collect();
-   
-    let raw_addr = args[1].to_string().clone();
+fn get_ip_from_raw_addr(raw_addr : &String) -> &Ipv4Addr {
 
     let ipv4 = IPv4Validator {
         port: ValidatorOption::NotAllow,
@@ -39,27 +40,51 @@ fn main() {
     }
 
     match ipv4.parse_string(raw_addr.clone()) {
-        Ok(ipv4_addr) => assert_eq!(raw_addr, ipv4_addr.get_full_ipv4()),
+        Ok(ipv4_addr) => {
+            assert_eq!(raw_addr, ipv4_addr.get_full_ipv4());
+            ipv4_addr.get_ipv4_address()
+        }
         Err(_) => match domain.parse_string(raw_addr.clone()) {
             Ok(domain) => {
                 //assert_eq!(raw_addr, domain.get_full_domain());
                 dns(domain)
+
             }
-            Err(_) => println!("Not valid ip or hostname")
+            Err(_) => panic!("Not valid ip or hostname")
         }
     }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+   
+    let raw_addr = args[1].to_string().clone();
+
+    let ip_addr = IpAddr::V4(*get_ip_from_raw_addr(&raw_addr));
+
 
     // Create a new transport channel, dealing with layer 4 packets on a test protocol
     // It has a receive buffer of 4096 bytes.
-    let (mut tx, mut rx) = match transport_channel(4096, Layer3(IpNextHeaderProtocols::Icmp)) {
+    let (mut sender, mut reciever) = match transport_channel(4096, Layer3(IpNextHeaderProtocols::Icmp)) {
         Ok((tx, rx)) => (tx, rx),
         Err(e) => panic!(
             "An error occurred when creating the transport channel: {}",
             e
         ),
     };
+    let mut vec = Vec::new();
+    vec.push(1);
 
-    
+    let packet = Icmp {
+        icmp_type : IcmpType(8),
+        icmp_code : IcmpCode(0),
+        checksum : 0,
+        payload : vec
+    };
+
+    sender.send_to(packet, ip_addr);
+
+
 }
 
 
